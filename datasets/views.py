378d2b6ecb6dd05e4bce018e3027dae4e5641ca7
mateusz28011 <mathew28011@gmail.com>
2021-11-21 18:43:39 +0100

    Adding action to show clustering groups of dataset

--- datasets/views.py
+++ datasets/views.py
@@ -1,6 +1,11 @@
+from core.utils import SwaggerOrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from ml.serializers import ClusteringSerializer
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from datasets.models import Dataset
 from datasets.permissions import IsOwner
@@ -11,12 +16,28 @@ class DatasetViewset(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet
 ):
     queryset = Dataset.objects.all()
-    parser_classes = [MultiPartParser]
+    # parser_classes = [MultiPartParser]
     serializer_class = DatasetSerializer
     permission_classes = [IsAuthenticated & IsOwner]
+    filter_backends = [DjangoFilterBackend, SwaggerOrderingFilter]
+    ordering_fields = ["created"]
+    ordering = ["-created"]
 
     def perform_create(self, serializer):
         if "name" not in self.request.data:
             serializer.save(owner=self.request.user, name=self.request.data["file"].name)
         else:
             serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=["get"])
+    def clusterings(self, request, *args, **kwargs):
+        instance = self.get_object()
+        clusterings = instance.clustering_set.all()
+
+        page = self.paginate_queryset(clusterings)
+        if page is not None:
+            serializer = ClusteringSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = ClusteringSerializer(instance, many=True)
+        return Response(serializer.data)
