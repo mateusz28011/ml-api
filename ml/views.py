5b7c91c8b7c1b78e6cebb4963ef6e8b66a3deaf6
mateusz28011 <mathew28011@gmail.com>
2021-12-09 23:50:22 +0100

    Handled choosing algorithms by ids

--- ml/views.py
+++ ml/views.py
@@ -1,4 +1,8 @@
+import json
+
+import pandas as pd
 from django.conf import settings
+from django.http.response import JsonResponse
 from django_celery_results.models import TaskResult
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
@@ -51,11 +55,29 @@ class AlgorithmDataViewset(
 
     def get_queryset(self):
         if self.action == "list":
-            queryset = AlgorithmData.objects.filter(clustering=self.kwargs["clustering_pk"])
+            # queryset = AlgorithmData.objects.filter(clustering=self.kwargs["clustering_pk"]
+            queryset = self.get_clustering().algorithmdata_set.all()
         else:
             queryset = self.queryset
 
         return queryset
+
+    def list(self, request, *args, **kwargs):
+        ids = request.query_params.getlist("ids", None)
+
+        if ids and len(ids) != 0:
+            queryset = self.get_queryset().filter(pk__in=ids)
+            serializer = self.serializer_class(queryset, many=True)
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+            serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -86,3 +108,14 @@ class AlgorithmDataViewset(
             instance.save()
 
         return Response("Started")
+
+    # @action(detail=True, methods=["get"])
+    # def (self, request, *args, **kwargs):
+    #     instance = self.get_object()
+
+    #     try:
+    #         points = pd.read_csv(instance.plot_2d_points).to_json(orient="values")
+    #     except:
+    #         raise NotFound()
+
+    #     return JsonResponse(json.loads(points), safe=False)
